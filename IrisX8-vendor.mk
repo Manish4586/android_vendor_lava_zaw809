141d738888c52b6142f3c4b6442d3a2c2b4db904
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-08 00:27:28 +0530

    First commit

---
+++ IrisX8-vendor.mk
@@ -0,0 +1,12 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lava/IrisX8/proprietary/xbin,system/xbin)
+
